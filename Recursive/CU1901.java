6927787ac6dc756a191faa033147c0e7f0c08639
hansun song <songhs-0601@naver.com>
2026-06-18 01:04:59 +0900

    Failing

--- Recursive/CU1901.java
+++ Recursive/CU1901.java
@@ -1,7 +1,20 @@
 package Recursive;
 
 public class CU1901 {
+
+    public static void StringReverse(String n) {
+        if(n.length() == 0) {
+            return;
+        }
+
+        else {
+            System.out.print(n.chatAt(n));
+            StringReverse(n - 1);
+        }
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println("TEST");
+        StringReverse("Hello, World!");
     }
 }
